37b750f8c459b98dc186760539a92e2f73413174
alekseisavinok6 <alekseisavinok6@gmail.com>
2024-11-21 13:36:38 +0100

    v1.3

--- noticias.php
+++ noticias.php
@@ -17,6 +17,7 @@ $usuario = $_SESSION['usuario_cookie'];
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Noticias personalizadas</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script>
         function verNoticia(idNoticia, titulo) {
